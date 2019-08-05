882001ffb83ccd5e03b94374aa1cc47ded08031e
Minsu <35769058+ChocoBerry12@users.noreply.github.com>
2019-08-05 16:10:15 +0900

    1

--- autotrading/pusher/base_pusher.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Pusher(ABC):
-    @abstractmethod
-    def send_message(self, thread, message):
-        pass
--- conf/README.md
+++
@@ -1,2 +0,0 @@
-#It's private information
-
